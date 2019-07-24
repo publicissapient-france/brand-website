3758a3acc510243ab30fe643448ff3cc6bd93364
Axel BOIZON <aboizon@MacBook-Air-de-xebia-2.local>
2019-07-24 10:44:04 +0200

    Logos page (actual logos are missing, but the template is finished)

--- logos.php
+++ logos.php
@@ -15,16 +15,16 @@
 				<h1>PUBLICIS SAPIENT ENGINEERING</h1>
 				<p>Tous les logos présents sur cette page sont au format <span>SVG</span></p>
 			</div>
-			<div class="logos-download-rectangle">
-				<div class="pse-logo-rectangle-download">
-					<img class="logo-preview-rectangle" src="../images/pse-logo.svg"/>
+			<div class="logos-download rectangle">
+				<div class="pse-logo-download rectangle">
+					<img class="logo-preview rectangle" src="../images/pse-logo.svg"/>
 					<a class="download-button">
 						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 					</a>
 				</div>
 				
-				<div class="pse-logo-rectangle-download white">
-					<img class="logo-preview-rectangle" src="../images/PSE-logo-white.svg"/>
+				<div class="pse-logo-download rectangle white">
+					<img class="logo-preview rectangle" src="../images/PSE-logo-white.svg"/>
 					<a class="download-button">
 						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 					</a>
@@ -34,32 +34,32 @@
 			<div class="logos-title">
 				<h1>PUBLICIS SAPIENT</h1>
 			</div>
-			<div class="logos-download-square">
+			<div class="logos-download square">
 				<div class="square-container">
-					<div class="pse-logo-square-download">
-						<img class="logo-preview-square" src="../images/manquelefichier.svg"/>
-						<a class="download-button">
+					<div class="pse-logo-download square">
+						<img class="logo-preview square" src="../images/manquelefichier.svg"/>
+						<a class="download-button square">
 							<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 						</a>
 					</div>
-					<div class="pse-logo-square-download">
-						<img class="logo-preview-square" src="../images/manquelefichier.svg"/>
-						<a class="download-button">
+					<div class="pse-logo-download square">
+						<img class="logo-preview square" src="../images/manquelefichier.svg"/>
+						<a class="download-button square">
 							<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 						</a>
 					</div>
 				</div>
 
 				<div class="square-container">
-					<div class="pse-logo-square-download white">
-						<img class="logo-preview-square" src="../images/manquelefichier.svg"/>
-						<a class="download-button">
+					<div class="pse-logo-download square white">
+						<img class="logo-preview square" src="../images/manquelefichier.svg"/>
+						<a class="download-button square">
 							<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 						</a>
 					</div>
-					<div class="pse-logo-square-download white">
-						<img class="logo-preview-square" src="../images/manquelefichier.svg"/>
-						<a class="download-button">
+					<div class="pse-logo-download square white">
+						<img class="logo-preview square" src="../images/manquelefichier.svg"/>
+						<a class="download-button square">
 							<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}</style></defs><rect class="a" width="50" height="50"/><path d="M1048.823,1009.25h-12.5v5h-9v16.5h16.5v-9h5Zm-6.5,20h-13.5v-13.5h7.5v6h6Zm0-9h-4.5v-4.5h4.5Zm5,0h-3.5v-6h-6v-3.5h9.5Z" transform="translate(-1013.072 -995)"/></svg>
 						</a>
 					</div>
