7846f843f27cab643be9f01ff4cbe4c9f3a52646
Kristof Kerninon <kristof@kerninon.com>
2020-01-08 15:31:28 +0100

    add link font

--- fonts.php
+++ fonts.php
@@ -27,8 +27,8 @@
 				<p>Les deux types de caractères, <span>Futura Next</span> et <span>Minion Pro</span>, sont utilisés pour tous les actifs de marque et sont disponibles au téléchargement. Pour vous assurer que les polices s’affichent correctement sur des ordinateurs autres que PS, nous vous recommandons de créer des PDF avant d’envoyer des présentations à des destinataires externes, y compris des clients.</p>
 			</div>
 			<div class="download-link">
-				<a class="download-box" href="">
-					<p><span class="red-text">O </span><span class="on-hover">PS_Fonts.zip</span><span class="text-file-size"> (1.5 Mo)</span></p>
+				<a class="download-box" href="https://drive.google.com/file/d/1dTUdI9OqqDckb3nogarI0pvtTUzKWxBC">
+					<p><span class="red-text">O </span><span class="on-hover">All Fonts</span><span class="text-file-size"> (1.6 Mo)</span></p>
 				</a>
 			</div>
 
@@ -43,8 +43,8 @@
 						<img src="./images/typo-futura2.png">
 					</div>
 					<div class="download-link">
-						<a class="download-box" href="">
-							<p><span class="red-text">O </span><span class="on-hover">PS_Fonts.zip</span><span class="text-file-size"> (1.5 Mo)</span></p>
+						<a class="download-box" href="https://drive.google.com/file/d/1NPQansbVjIDblVG5Yb5Ibo69B2SVgGmK/view?usp=sharing">
+							<p><span class="red-text">O </span><span class="on-hover">Futura Next</span><span class="text-file-size"> (339 ko)</span></p>
 							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21.5" height="21.5" viewBox="0 0 21.5 21.5"><defs><style>.a{fill:none;}.b{clip-path:url(#a);}.c{fill:#fe414d;}</style><clipPath id="a"><rect class="a" width="21.5" height="21.5"/></clipPath></defs><g class="b"><path class="c" d="M947.681,1009.25v21.5h21.5v-21.5Zm20,20h-18.5v-18.5h18.5Z" transform="translate(-947.681 -1009.25)"/><path class="c" d="M964.961,1020.53l-1.061-1.061-4.72,4.72V1014h-1.5v10.189l-4.72-4.72-1.061,1.061,6.53,6.53Z" transform="translate(-947.681 -1009.25)"/></g></svg>
 						</a>
 					</div>
@@ -60,8 +60,8 @@
 						<img src="./images/typo-minion2.png">
 					</div>
 					<div class="download-link">
-						<a class="download-box" href="">
-							<p><span class="red-text">O </span><span class="on-hover">PS_Fonts.zip</span><span class="text-file-size"> (1.5 Mo)</span></p>
+						<a class="download-box" href="https://drive.google.com/open?id=1B6O9mB1wj13XD2GvxU5Lf9kW0nww2985">
+							<p><span class="red-text">O </span><span class="on-hover">Minion Pro</span><span class="text-file-size"> (1.3 Mo)</span></p>
 							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21.5" height="21.5" viewBox="0 0 21.5 21.5"><defs><style>.a{fill:none;}.b{clip-path:url(#a);}.c{fill:#fe414d;}</style><clipPath id="a"><rect class="a" width="21.5" height="21.5"/></clipPath></defs><g class="b"><path class="c" d="M947.681,1009.25v21.5h21.5v-21.5Zm20,20h-18.5v-18.5h18.5Z" transform="translate(-947.681 -1009.25)"/><path class="c" d="M964.961,1020.53l-1.061-1.061-4.72,4.72V1014h-1.5v10.189l-4.72-4.72-1.061,1.061,6.53,6.53Z" transform="translate(-947.681 -1009.25)"/></g></svg>
 						</a>
 					</div>
