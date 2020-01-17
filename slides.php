6dd2deb01c33619ff847c0e0379de2f3cee53491
Kristof Kerninon <kristof@kerninon.com>
2020-01-17 15:36:30 +0100

    modified assets and slides

--- slides.php
+++ slides.php
@@ -1,97 +1,102 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Brand - Slides</title>
-		<?php include('header.html'); ?>
-		<link rel="stylesheet" type="text/css" href="./styles/banner.css">
-		<link rel="stylesheet" type="text/css" href="./styles/slides.css">
-	</head>
-	<body>
-		<?php $page = "slides"; ?>
-		<?php include('topbar.html');?>
+<head>
+    <title>Brand - Slides</title>
+    <?php include('header.html'); ?>
+    <link rel="stylesheet" type="text/css" href="./styles/banner.css">
+    <link rel="stylesheet" type="text/css" href="./styles/slides.css">
+</head>
+<body>
+<?php $page = "slides"; ?>
+<?php include('topbar.html');?>
 
-		<div class="banner">
-			<div class="banner-content">
-				<div class="banner-logo">
-					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}.b{clip-path:url(#a);}</style><clipPath id="a"><rect class="a" width="50" height="50"/></clipPath></defs><g class="b"><rect class="a" width="50" height="50"/><g transform="translate(-666.056 -995)"><path d="M680.306,1009.25v21.5h21.5v-21.5Zm20,20h-18.5v-18.5h18.5Z"/><path d="M698.806,1014.25h-15.5v11.5h15.5Zm-1.5,10h-12.5v-8.5h12.5Z"/></g></g></svg>
-				</div>
-				<h1>CORPORATE</h1>
-			</div>
-		</div>
+<div class="banner">
+    <div class="banner-content">
+        <div class="banner-logo">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 50 50"><defs><style>.a{fill:none;}.b{clip-path:url(#a);}</style><clipPath id="a"><rect class="a" width="50" height="50"/></clipPath></defs><g class="b"><rect class="a" width="50" height="50"/><g transform="translate(-666.056 -995)"><path d="M680.306,1009.25v21.5h21.5v-21.5Zm20,20h-18.5v-18.5h18.5Z"/><path d="M698.806,1014.25h-15.5v11.5h15.5Zm-1.5,10h-12.5v-8.5h12.5Z"/></g></g></svg>
+        </div>
+        <h1>CORPORATE</h1>
+    </div>
+</div>
 
-		<div class="content">
+<div class="content">
 
-			<div class="card-container">
-			 
-				<div class="card">
-				<div class="cercle cercle-socialmedia"></div>
-					<h2>Réseaux Sociaux</h2>
-					<h3>JPG & PNG</h3>
-					<div class="liste">
-						<ul>
-							<li><a href="https://drive.google.com/open?id=1U1sZuOuLPUTXChAKXi4HV0UB9mk7KIQq" target="new">Pictos</a></li>
-							<li><a href="https://drive.google.com/open?id=1l7WeMraVKE0LhFznzbR1EvmbJ6KKB7M0" target="new">Bannières</a></li>
-						</ul>
-					</div>
-				</div>
+    <div class="card-container">
 
-				<div class="card">
-				<div class="cercle cercle-templateslide"></div>
-					<h2>Template Slide</h2>
-					<h3>GSLIDE</h3>
-					<div class="liste">
-						<ul class="un">
-							<li><a href="https://drive.google.com/open?id=1VWHBjyLK2Zi4qeurkZmk0lv2gG5_RI6p" target="new">Talk</a></li>
-							<li><a href="https://drive.google.com/open?id=1EbLQLC-avL5p0fcq0hCwHP-o94CHB6np" target="new">Corporate</a></li>
-						</ul>
-					</div>
-				</div>
+        <div class="card">
+            <div class="cercle cercle-socialmedia"></div>
+            <h2>Réseaux Sociaux</h2>
+            <h3>JPG & PNG</h3>
+            <div class="liste">
+                <ul>
+                    <li><a href="https://drive.google.com/open?id=1U1sZuOuLPUTXChAKXi4HV0UB9mk7KIQq" target="new">Pictos</a></li>
+                    <li><a href="https://drive.google.com/open?id=1l7WeMraVKE0LhFznzbR1EvmbJ6KKB7M0" target="new">Bannières</a></li>
+                </ul>
+            </div>
+        </div>
 
-				<div class="card">
-				<div class="cercle cercle-whois"></div>
-					<h2>Whois</h2>
-					<h3>JPG & GSLIDE</h3>
-					<div class="liste">
-						<ul class="un">
-							<li><a href="https://drive.google.com/open?id=1A-w14mrdMcevFlL0uKaLyjweyLcDxzav" target="new">Photos</a></li>
-							<li><a href="https://drive.google.com/open?id=1Fpv0rJ9xM2SnckBqpy1lsp6U_iu9h1r1" target="new">CV</a></li>
-						</ul>
-					</div>
-				</div>
+        <div class="card">
+            <div class="cercle cercle-templateslide"></div>
+            <h2>Template Slide</h2>
+            <h3>GSLIDE</h3>
+            <div class="liste">
+                <ul class="un">
+                    <li class="blue"><!--<li><a href="https://drive.google.com/open?id=1VWHBjyLK2Zi4qeurkZmk0lv2gG5_RI6p" target="new">-->Talk</li>
+                    <li><a href="https://drive.google.com/open?id=1EbLQLC-avL5p0fcq0hCwHP-o94CHB6np" target="new">Corporate</a></li>
+                </ul>
+            </div>
+        </div>
 
-				<div class="card large">
-				<div class="cercle cercle-signature"></div>
-					<h2>Signature</h2>
-					<h3>JPG & PNG</h3>
-					<div class="liste">
-						<ul class="un">
-							<li><a href="https://drive.google.com/open?id=1mqQUDeMz1-0MbnS6oOHGYLCTMR7lLVTs" target="new">Mail Corporate</a></li>
-							<li><a href="https://drive.google.com/open?id=1vEa_Gr6QsDBIw3y_bANyxid6dRoXi6AU" target="new">Events</a></li>
-							<li><a href="https://drive.google.com/open?id=1apuF283VIgUxFAXkEczNDnIzX-JDIf7s" target="new">Annonces</a></li>
-						</ul>
-					</div>
-				</div>
+        <div class="card">
+            <div class="cercle cercle-whois"></div>
+            <h2>Whois</h2>
+            <h3>JPG & GSLIDE</h3>
+            <div class="liste">
+                <ul class="un">
+                    <li><a href="https://drive.google.com/open?id=1A-w14mrdMcevFlL0uKaLyjweyLcDxzav" target="new">Photos</a></li>
+                    <li><a href="https://drive.google.com/open?id=1Fpv0rJ9xM2SnckBqpy1lsp6U_iu9h1r1" target="new">CV</a></li>
+                </ul>
+            </div>
+        </div>
 
-				<div class="card large">
-				<div class="cercle cercle-techtrends"></div>
-					<h2>Techtrends</h2>
-					<h3>JPG & PNG</h3>
-					<div class="liste">
-						<ul class="un">
-							<li><a href="https://drive.google.com/open?id=1FjVWoyvVtNBm6wP7UMm6f3N7ymlZ8J1R" target="new">Covers</a></li>
-							<li><a href="https://drive.google.com/open?id=1jrJ2kg-MDa7rPjdtEQorUrVpzbIvVtn8" target="new">PDF</a></li>
-							<li><a href="https://drive.google.com/open?id=1zzR8a_taJPHDAtKD1QGTPq5T_wqM6q-t" target="new">Illustrations</a></li>
-							<li><a href="https://drive.google.com/open?id=1Wz5rHpHq5Lhoo5vG26cIs83fh5k-UCx6" target="new">Image Bank</a></li>
-						</ul>
-					</div>
-				</div>
+        <div class="card large">
+            <div class="cercle cercle-signature"></div>
+            <h2>Signature</h2>
+            <h3>JPG & PNG</h3>
+            <div class="liste">
+                <ul class="un">
+                    <li><a href="https://drive.google.com/open?id=1mqQUDeMz1-0MbnS6oOHGYLCTMR7lLVTs" target="new">Mail Corporate</a></li>
+                    <li class="blue">Events</li>
+                    <li class="blue">Annonces</li>
+                </ul>
+            </div>
+        </div>
 
-				<div class="card last"></div>
+        <div class="card large">
+            <div class="cercle cercle-techtrends"></div>
+            <h2>Techtrends</h2>
+            <h3>JPG & PNG</h3>
+            <div class="liste">
+                <ul class="un">
+                    <li><a href="https://drive.google.com/open?id=1FjVWoyvVtNBm6wP7UMm6f3N7ymlZ8J1R" target="new">Covers</a></li>
+                    <li><a href="https://drive.google.com/open?id=1jrJ2kg-MDa7rPjdtEQorUrVpzbIvVtn8" target="new">PDF</a></li>
+                    <li><a href="https://drive.google.com/open?id=1zzR8a_taJPHDAtKD1QGTPq5T_wqM6q-t" target="new">Illustrations</a></li>
+                    <li><a href="https://drive.google.com/open?id=1Wz5rHpHq5Lhoo5vG26cIs83fh5k-UCx6" target="new">Image Bank</a></li>
+                </ul>
+            </div>
+        </div>
 
-			</div>
+        <div class="card last"></div>
 
-		</div>
+    </div>
+    <div>
+        <ul class="un">
+            <li class="bullet-blue">C'est pour bientôt !</li>
+        </ul>
+    </div>
 
-		<?php include('footer.php');?>
-	</body>
+</div>
+
+<?php include('footer.php');?>
+</body>
 </html>
